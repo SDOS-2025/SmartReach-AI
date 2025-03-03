d99875dba72eaa7c2b2da7e3f4805f7bbf3a5a6b
noeltiju <noelab04@gmail.com>
2025-03-03 19:51:39 +0530

    Tested connection

--- frontend/app/components/NavigationMenu.tsx
+++ frontend/app/components/NavigationMenu.tsx
@@ -1,7 +1,26 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
+import axios from 'axios'
+import { useState } from "react";
+
+
 
 function NavigationMenu() {
+
+  const [message, setMessage] = useState("");
+
+  const fetchData = async () => {
+    try {
+        const response = await axios.get("http://127.0.0.1:8000/api/hello/");
+        setMessage(response.data.message);
+        console.log("API Response:", response.data.message); // ✅ Log message to console
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        setMessage("Failed to fetch data.");
+    }
+  };
   return (
     <nav className="flex items-center justify-between px-6 py-4 h-full w-full">
     {/* Left Section */}
@@ -25,6 +44,17 @@ function NavigationMenu() {
           Login
         </button>
       </Link>
+
+      <Link href="">
+          <button
+              onClick={fetchData}
+              className="px-4 py-2 bg-blue-600 text-white rounded-md text-sm hover:bg-blue-700 transition-colors"
+          >
+              Register
+          </button>
+      </Link>
+
+
       
       {/* User Profile */}
       {/* <div className="flex items-center gap-2">
