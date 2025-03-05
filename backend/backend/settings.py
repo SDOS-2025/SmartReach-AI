ddd02f0e51b60076c614195d01ce491237449856
noeltiju <noelab04@gmail.com>
2025-03-05 13:29:10 +0530

    Updated django settings.py for new postgresql instance

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
 # Import settings from oauth_settings.py
-from .oauth_settings import GOOGLE_OAUTH2_CLIENT_ID, GOOGLE_OAUTH2_CLIENT_SECRET
+from .oauth_settings import GOOGLE_OAUTH2_CLIENT_ID, GOOGLE_OAUTH2_CLIENT_SECRET, host, user, password, database, port
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,8 +136,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': database,
+        'USER': user,
+        'PASSWORD': password,
+        'HOST': host,
+        'PORT': port,
     }
 }
 
--- database_test/setup.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-import config
-
-# Load database connection parameters
-params = config.config()
-
-# Connect to PostgreSQL
-connection = psycopg2.connect(**params)
-connection.autocommit = True
